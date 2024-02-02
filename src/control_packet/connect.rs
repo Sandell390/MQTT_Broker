bd39f2c0cfc51714451ec6d397a24d2bb29dae82
Marcus Jensen <marcusproman0507@gmail.com>
2024-02-02 11:11:10 +0100

    A bunch of ifs

--- src/control_packet/connect.rs
+++ src/control_packet/connect.rs
@@ -55,6 +55,53 @@ pub fn validate(buffer: [u8; 8192], bytes_read: usize) -> Result<[u8; 4], &'stat
     let flag_6: bool = (byte & 0b01000000) != 0; // Password Flag
     let flag_7: bool = (byte & 0b10000000) != 0; // Username Flag
 
+    // Reserved flag
+    if flag_0 {
+        return Err("Reserved flag is not 0");
+    }
+
+    // Clean session flag
+    if flag_1 {
+        // If CleanSession is set to 1, the Client and Server MUST discard any previous Session and start a new one.
+        // This Session lasts as long as the Network Connection.
+        // State data associated with this Session MUST NOT be reused in any subsequent Session.
+    } else {
+        // If CleanSession is set to 0, the Server MUST resume communications with the Client based on state from the current Session (as identified by the Client identifier).
+        // If there is no Session associated with the Client identifier the Server MUST create a new Session.
+        // The Client and Server MUST store the Session after the Client and Server are disconnected.
+        // After the disconnection of a Session that had CleanSession set to 0, the Server MUST store further QoS 1 and QoS 2 messages that match any subscriptions that the client had at the time of disconnection as part of the Session state.
+    }
+
+    // Will flag
+    if flag_2 {
+        // Code goes here
+    }
+
+    // QoS 1 flag
+    if flag_3 {
+        // Code goes here
+    }
+
+    // QoS 2 flag
+    if flag_4 {
+        // Code goes here
+    }
+
+    // Will Retain flag
+    if flag_5 {
+        // Code goes here
+    }
+
+    // Password flag
+    if flag_6 {
+        // Code goes here
+    }
+
+    // Username flag
+    if flag_7 {
+        // Code goes here
+    }
+
     // current_index += 1;
 
     // Assemble return packet
