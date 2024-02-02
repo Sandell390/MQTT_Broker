1439ac1eddc6d183c63ec1c670d03d5e4b1811b5
Jesper Sandell <sandell327@gmail.com>
2024-02-02 12:57:07 +0100

    Started on subscribe validation

--- src/control_packet/subcribe.rs
+++ src/control_packet/subcribe.rs
@@ -1,5 +1,39 @@
-pub fn validate(){
+use crate::common_fn;
+
+pub fn validate(buffer: [u8; 8192], bytes_read: usize) -> Result<(u8, Vec<String>), &'static str>{
 
 
+    let mut remaining_length: usize = 0;
+
+    match common_fn::bit_operations::decode_remaining_length(&buffer) {
+        Ok(value) => {
+            remaining_length = value;
+        }
+        Err(err) => println!("Error: {}", err),
+    }
+
+    // Test if the first byte have second bit is on
+    match common_fn::bit_operations::split_byte(&buffer[0], 4) {
+        Ok(value) => {
+
+            if value[1] != 2{
+                return Err("The first byte have second bit is on");
+            }
+        }
+        Err(err) => println!("Error: {}",err ),
+    }
+
+    // Get Packet ID
     
+
+
+
+
+
+
+
+    // Get all topic filters
+
+
+    return Ok((0,Vec::new()));
 }
